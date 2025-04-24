81c1e9553df3adea7596678867b1ae098eac5e86
Hamdi Shehab <7amdishehab@gmail.com>
2025-04-24 22:28:06 +0200

    Update Prayer.jsx

--- src/components/Prayer.jsx
+++ src/components/Prayer.jsx
@@ -4,7 +4,7 @@ function Prayer({ name, time }) {
   return (
     <div className="w-full flex justify-between items-center font-semibold md:font-bold py-2 px-4 bg-amber-100 text-amber-900 rounded-sm">
       <p>{name}</p>
-      <p className="border-t-1 border-white">{time}</p>
+      <p className="">{time}</p>
     </div>
   );
 }
